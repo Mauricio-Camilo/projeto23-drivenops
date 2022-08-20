0e7452d13152b7fbb2c3759f4a8123e75d586629
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-08-20 10:50:30 -0500

    update: add proxy reverse configuration

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,3 +4,9 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.querySelector(".root"));
+
+// Endereço para funcionar na máquina local
+// REACT_APP_BASE_URL=http://localhost:5000
+
+// Endereço para funcionar na AWS
+// REACT_APP_BASE_URL=http://ec2-54-172-0-67.compute-1.amazonaws.com/api
